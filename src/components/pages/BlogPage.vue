1f006cdbbef49e83bebe8953d950cb58c68f2a6f
NataliaOrlova <natakarelskaya@yandex.ru>
2023-12-15 20:43:46 +0400

    blog page

---
+++ src/components/pages/BlogPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <BannerComp :banner-info="bannerInfo"/>
+  </div>
+</template>
+
+<script>
+import BannerComp from '../components/BannerComp.vue'
+
+export default {
+  name: 'BlogPage',
+
+  data () {
+    return {
+      bannerInfo: {
+        banner: 'img/blog_img_placeholder.jpg',
+        bannerTitle: 'Articles & News',
+        bannerLinks: ['Home', 'Blog']
+      }
+    }
+  },
+
+  mounted () {},
+
+  methods: {},
+  components: { BannerComp }
+}
+</script>
+
+<style lang="scss" scoped></style>
