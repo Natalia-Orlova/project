bafd1d6004c7a7144bfc52b22fe152b92ee20f6f
NataliaOrlova <natakarelskaya@yandex.ru>
2024-01-25 19:25:27 +0400

    redirected articles and news to /blog

--- src/components/components/NewsItem.vue
+++ src/components/components/NewsItem.vue
@@ -2,10 +2,10 @@
   <div class="blog-news__item">
     <img class="blog-news__item_img" :src=newsData.img alt="news_photo">
     <p class="blog-news__item_theme">{{ newsData.theme }}</p>
-    <router-link to="/404" class="blog-news__item_title">{{ newsData.title }}</router-link>
+    <router-link to="/blog" class="blog-news__item_title">{{ newsData.title }}</router-link>
     <div class="blog-news__item_date">
       <p class="blog-news__item_datetext">{{ newsData.date }}</p>
-      <router-link to="/404" class="blog-news__item_next"><svg width="10" height="20" viewBox="0 0 10 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <router-link to="/blog" class="blog-news__item_next"><svg width="10" height="20" viewBox="0 0 10 20" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M1 19L9 10L1 1" stroke="#292F36" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
       </svg></router-link>
     </div>
